0b6cb6fc8f1937e866fcd6ff7b2efee8d929235d
EliottDvx <eliottdevaux@gmail.com>
2023-03-03 11:18:39 +0100

    wip exo 5 TP2

--- TP2/exo5.cpp
+++ TP2/exo5.cpp
@@ -9,21 +9,52 @@ void merge(Array& first, Array& second, Array& result);
 void splitAndMerge(Array& origin)
 {
 	// stop statement = condition + return (return stop the function even if it does not return anything)
+    if(origin.size() == 1){
+        return;
+    }
 
 	// initialisation
 	Array& first = w->newArray(origin.size()/2);
 	Array& second = w->newArray(origin.size()-first.size());
 	
-	// split
+    for(uint i = 0; i < origin.size(); i++){
+        if(i < first.size()){
+            first[i] = origin[i];
+        }
+        else{
+            second[i-first.size()] = origin[i];
+        }
+    }
 
-	// recursiv splitAndMerge of lowerArray and greaterArray
 
-	// merge
+    splitAndMerge(first);
+    splitAndMerge(second);
+
+    merge(first, second, origin);
 }
 
 void merge(Array& first, Array& second, Array& result)
 {
-
+    if(first[0] < second[0]){
+        for(uint i = 0; i < result.size(); i++){
+            if(i < first.size()){
+                result[i] = first[i];
+            }
+            else{
+                result[i] = second[i-first.size()];
+            }
+        }
+    }
+    else{
+        for(uint i = 0; i < result.size(); i++){
+            if(i < second.size()){
+                result[i] = second[i];
+            }
+            else{
+                result[i] = first[i-second.size()];
+            }
+        }
+    }
 }
 
 void mergeSort(Array& toSort)
