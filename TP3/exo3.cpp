da40e76143a5c25b32929ee6961b15b2b4b5f21f
EliottDvx <eliottdevaux@gmail.com>
2023-03-17 10:04:54 +0100

    exo 3 TP3

--- TP3/exo3.cpp
+++ TP3/exo3.cpp
@@ -41,56 +41,115 @@ struct SearchTreeNode : public Node
     }
 
     uint height() const	{
-        if(this->left != nullptr && this->right != nullptr){
-            if(this->left->height() > this->right->height()){
-                return (this->left->height() + 1);
-            }
-            else{
-                return (this->right->height() + 1);
-            }
+        int heightLeft = 0;
+        int heightRight = 0;
+
+        if(this->left != nullptr){
+           heightLeft = this->left->height();
         }
-        else{
-            if(this->left != nullptr){
-                return (this->left->height() + 1);
-            }
-            else if(this->right != nullptr){
-                return (this->right->height() + 1);
-            }
+
+        if(this->right != nullptr){
+            heightRight = this->right->height();;
+        }
+
+        if(heightLeft < heightRight){
+            return heightRight+1;
+        }else{
+            return heightLeft+1;
         }
 
         return 1;
     }
 
 	uint nodesCount() const {
-        // should return the sum of nodes within left child and
-        // right child +1 for itself. If there is no child, return
-        // just 1
-        return 1;
+        int countLeft = 0;
+        int countRight = 0;
+
+        if(this->left != nullptr){
+           countLeft = this->left->nodesCount();
+        }
+
+        if(this->right != nullptr){
+            countRight = this->right->nodesCount();;
+        }
+
+        return countLeft + countRight + 1;
 	}
 
 	bool isLeaf() const {
-        // return True if the node is a leaf (it has no children)
+        if(this->left == nullptr && this->right == nullptr){
+            return true;
+        }
         return false;
 	}
 
 	void allLeaves(Node* leaves[], uint& leavesCount) {
-        // fill leaves array with all leaves of this tree
-	}
+        if(this->isLeaf() == true){
+            leaves[leavesCount] = this;
+            leavesCount++;
+        }else{
+            if(this->left != nullptr){
+               this->left->allLeaves(leaves, leavesCount);
+            }
+
+            if(this->right != nullptr){
+                this->right->allLeaves(leaves, leavesCount);
+            }
+        }
+    }
 
 	void inorderTravel(Node* nodes[], uint& nodesCount) {
-        // fill nodes array with all nodes with inorder travel
+        if(this->left != nullptr){
+            this->left->inorderTravel(nodes, nodesCount);
+        }
+
+        nodes[nodesCount] = this;
+        nodesCount++;
+
+        if(this->right != nullptr){
+            this->right->inorderTravel(nodes, nodesCount);
+        }
 	}
 
 	void preorderTravel(Node* nodes[], uint& nodesCount) {
-        // fill nodes array with all nodes with preorder travel
+        nodes[nodesCount] = this;
+        nodesCount++;
+
+        if(this->left != nullptr){
+            this->left->preorderTravel(nodes, nodesCount);
+        }
+
+        if(this->right != nullptr){
+            this->right->preorderTravel(nodes, nodesCount);
+        }
 	}
 
 	void postorderTravel(Node* nodes[], uint& nodesCount) {
-        // fill nodes array with all nodes with postorder travel
+        if(this->left != nullptr){
+            this->left->postorderTravel(nodes, nodesCount);
+        }
+
+        if(this->right != nullptr){
+            this->right->postorderTravel(nodes, nodesCount);
+        }
+
+        nodes[nodesCount] = this;
+        nodesCount++;
 	}
 
 	Node* find(int value) {
-        // find the node containing value
+        if(this->value == value){
+            return this;
+        }
+
+        if(this->left != nullptr){
+            return this->left->find(value);
+        }
+
+        if(this->right != nullptr){
+            return this->right->find(value);
+        }
+
 		return nullptr;
 	}
 
