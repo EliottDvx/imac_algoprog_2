57e2fbc9ce6968db97f0ee3f85bbafcbc167224b
EliottDvx <eliottdevaux@gmail.com>
2023-03-24 10:30:09 +0100

    wip exo 1 TP4

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -11,18 +11,21 @@ using std::string;
 
 int Heap::leftChildIndex(int nodeIndex)
 {
-    return 0;
+    return nodeIndex*2 + 1;
 }
 
 int Heap::rightChildIndex(int nodeIndex)
 {
-    return 0;
+    return nodeIndex*2 +2;
 }
 
 void Heap::insertHeapNode(int heapSize, int value)
 {
-	// use (*this)[i] or this->get(i) to get a value at index i
 	int i = heapSize;
+    while(i>0 && (*this)[i] > (*this)[(i-1)/2]){
+        swap( (*this)[i], (*this)[(i-1)/2] );
+        i = (i-1)/2;
+    }
 }
 
 void Heap::heapify(int heapSize, int nodeIndex)
