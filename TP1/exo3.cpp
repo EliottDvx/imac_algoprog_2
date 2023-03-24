e8a72ea5726788f017cd513d35128d95052fa6fc
EliottDvx <eliottdevaux@gmail.com>
2023-03-24 14:53:18 +0100

    exo 3 TP1

--- TP1/exo3.cpp
+++ TP1/exo3.cpp
@@ -9,9 +9,17 @@ int search(int value, Array& toSort, int size)
 {
     Context _("search", value, size); // do not care about this, it allow the display of call stack
 
-    // your code
-    // check the last cell and if it does not correspond
-    // then consider a smaller array when recalling search
+    size--;
+
+    if(size < 0){
+        return_and_display(-1);
+    }
+
+    if(toSort[size] != value){
+        return_and_display(search(value, toSort, size));
+    }
+
+    return_and_display(size);
 }
 
 int main(int argc, char *argv[])
