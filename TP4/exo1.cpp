9c2a73079b1c039ff8f87f2c683ebdca5f3ba7aa
EliottDvx <eliottdevaux@gmail.com>
2023-05-30 17:06:56 +0200

    exo 1 TP4

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -22,26 +22,43 @@ int Heap::rightChildIndex(int nodeIndex)
 void Heap::insertHeapNode(int heapSize, int value)
 {
 	int i = heapSize;
+    (*this)[i] = value;
     while(i>0 && (*this)[i] > (*this)[(i-1)/2]){
-        swap( (*this)[i], (*this)[(i-1)/2] );
+        swap(i, (i-1)/2);
         i = (i-1)/2;
     }
 }
 
 void Heap::heapify(int heapSize, int nodeIndex)
 {
-	// use (*this)[i] or this->get(i) to get a value at index i
-	int i_max = nodeIndex;
+    int i_max = nodeIndex;
+
+    if(leftChildIndex(nodeIndex) < heapSize && (*this)[nodeIndex] < (*this)[leftChildIndex(nodeIndex)]){
+        i_max = leftChildIndex(nodeIndex);
+    }
+    if(rightChildIndex(nodeIndex) < heapSize && (*this)[i_max] < (*this)[rightChildIndex(nodeIndex)]){
+        i_max = rightChildIndex(nodeIndex);
+    }
+
+    if(i_max != nodeIndex){
+        swap(nodeIndex, i_max);
+        heapify(heapSize, i_max);
+    }
 }
 
 void Heap::buildHeap(Array& numbers)
 {
-
+    for(int i=0; i < numbers.size(); i++){
+        this->insertHeapNode(i, numbers[i]);
+    }
 }
 
 void Heap::heapSort()
 {
-
+    for(int i = (*this).size()-1; i > 0; i--){
+        swap(0, i);
+        heapify(i, 0);
+    }
 }
 
 int main(int argc, char *argv[])
